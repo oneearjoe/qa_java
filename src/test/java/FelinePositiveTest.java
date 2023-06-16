240710d41220717de173c2ead47cdc9b598d95dc
Nikita Bolgov <oneearjoe@yandex.ru>
2023-06-16 20:01:18 +0300

    Moved Feline parameterized test to new class

--- src/test/java/FelinePositiveTest.java
+++ src/test/java/FelinePositiveTest.java
@@ -1,29 +1,10 @@
 import com.example.Feline;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
+
 public class FelinePositiveTest {
-    private final int expectedKittensCount;
-    private final int kittensCount;
-
-    public FelinePositiveTest(int expectedKittensCount, int kittensCount) {
-        this.expectedKittensCount = expectedKittensCount;
-        this.kittensCount = kittensCount;
-    }
-
-    @Parameterized.Parameters(name = "ТестДата: Количество котят: {0}, {1}")
-    public static Object[][] params() {
-        return new Object[][]{
-                {1, 1},
-                {2, 2},
-        };
-    }
 
     @Test
     public void checkFelineEatsMeat() throws Exception {
@@ -37,9 +18,4 @@ public class FelinePositiveTest {
         assertEquals("Кошачьи", feline.getFamily());
     }
 
-    @Test
-    public void checkGetKittensReturnsKittenCount() {
-        Feline feline = new Feline();
-        assertEquals(expectedKittensCount, feline.getKittens(kittensCount));
-    }
 }
